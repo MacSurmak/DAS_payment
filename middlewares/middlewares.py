4100b4b5f0b5b6453b7431af25d37adc2f010e4d
macsurmak <macsurmak.m02@mail.ru>
2024-08-18 15:53:50 +0300

    Throttling middleware

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -1,7 +1,9 @@
-from typing import Any, Awaitable, Callable, Dict
+from typing import Any, Awaitable, Callable, Dict, Union
 from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery, TelegramObject
 from aiogram.fsm.storage.redis import RedisStorage
+from pyexpat.errors import messages
+
 from lexicon.lexicon import lexicon
 
 class MessageThrottlingMiddleware(BaseMiddleware):
@@ -11,17 +13,35 @@ class MessageThrottlingMiddleware(BaseMiddleware):
 
     async def __call__(self,
                        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-                       event: Message,
+                       event: Union[Message, CallbackQuery],
                        data: Dict[str, Any]) -> Any:
+
+        if type(event) is Message:
+            message = event
+        else:
+            message = event.message
 
         user = str(event.from_user.id)
         check_user = await self.storage.redis.get(name=user)
 
         if check_user:
-            if int(check_user.decode()) == 1:
-                await self.storage.redis.set(name=user, value=0, ex=1)
+            if check_user == 1:
+                await self.storage.redis.set(name=user, value=2, ex=3)
+                return await handler(event, data)
+            elif check_user == 2:
+                await self.storage.redis.set(name=user, value=3, ex=10)
                 return await event.answer(lexicon('throttling-warning'))
-            return
-        await self.storage.redis.set(name=user, value=1, ex=1)
+            elif check_user == 3:
+                return
 
+        await self.storage.redis.set(name=user, value=1, ex=3)
         return await handler(event, data)
+
+        # if check_user:
+        #     if int(check_user.decode()) == 1:
+        #         await self.storage.redis.set(name=user, value=0, ex=3)
+        #         return await event.answer(lexicon('throttling-warning'))
+        #     return
+        #
+        # await self.storage.redis.set(name=user, value=1, ex=1)
+        # return await handler(event, data)
