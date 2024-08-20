13846ad7fb81815f96cb10eeed827f8b84dc1966
macsurmak <macsurmak.m02@mail.ru>
2024-08-20 03:30:06 +0300

    window-dependent markup

--- handlers/admin.py
+++ handlers/admin.py
@@ -199,39 +199,39 @@ async def day(callback: CallbackQuery):
 @router.message(Command('action'))
 async def action(message: Message, bot: Bot):
 
-    users26 = read(table='Timetable',
-         columns='by_user',
-                 day=26,
-                 month=8,
-                   signed=1)
+    # users26 = read(table='Timetable',
+    #      columns='by_user',
+    #              day=26,
+    #              month=8,
+    #                signed=1)
+    #
+    # users27 = read(table='Timetable',
+    #      columns='by_user',
+    #              day=27,
+    #              month=8,
+    #                signed=1)
+    #
+    # lt = []
+    # for user in users26:
+    #     time = read(table='Timetable',
+    #                 columns='hour, minute',
+    #                 by_user=user[0],
+    #                 fetch=1)
+    #     if time[0] > 13:
+    #         await bot.send_message(chat_id=user[0], text=f'Время твоей записи перенесено в связи с изменением расписания паспортного стола. '
+    #                                                      f'Новое время — {str(time[0]).zfill(2)}:{str(time[1]).zfill(2)} '
+    #                                                      f'Приносим извинения за доставленные неудобства.')
+    #         lt.append([user[0], time])
+    #
+    # for user in users27:
+    #     time = read(table='Timetable',
+    #                 columns='hour, minute',
+    #                 by_user=user[0],
+    #                 fetch=1)
+    #     if time[0] > 13:
+    #         await bot.send_message(chat_id=user[0], text=f'Время твоей записи перенесено в связи с изменением расписания паспортного стола. '
+    #                                                      f'Новое время — {str(time[0]).zfill(2)}:{str(time[1]).zfill(2)} '
+    #                                                      f'Приносим извинения за доставленные неудобства.')
+    #         lt.append([user[0], time])
 
-    users27 = read(table='Timetable',
-         columns='by_user',
-                 day=27,
-                 month=8,
-                   signed=1)
-
-    lt = []
-    for user in users26:
-        time = read(table='Timetable',
-                    columns='hour, minute',
-                    by_user=user[0],
-                    fetch=1)
-        if time[0] > 13:
-            await bot.send_message(chat_id=user[0], text=f'Время твоей записи перенесено в связи с изменением расписания паспортного стола. '
-                                                         f'Новое время — {str(time[0]).zfill(2)}:{str(time[1]).zfill(2)} '
-                                                         f'Приносим извинения за доставленные неудобства.')
-            lt.append([user[0], time])
-
-    for user in users27:
-        time = read(table='Timetable',
-                    columns='hour, minute',
-                    by_user=user[0],
-                    fetch=1)
-        if time[0] > 13:
-            await bot.send_message(chat_id=user[0], text=f'Время твоей записи перенесено в связи с изменением расписания паспортного стола. '
-                                                         f'Новое время — {str(time[0]).zfill(2)}:{str(time[1]).zfill(2)} '
-                                                         f'Приносим извинения за доставленные неудобства.')
-            lt.append([user[0], time])
-
-    await message.answer(text=f'{lt}')
+    await message.answer(text=f'Нет никаких действий')
